cbb1adbd1a4f72f958fb37cc4da5ae0d1f5f60ad
vasilis-tsatalis <tsatalis.vas@gmail.com>
2021-09-24 00:26:49 +0300

    Initialize Application

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # python-api-auth
 Basic API Authentication
+
+# Run steps
+
+$ git clone 'project-url'
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+$ uvicorn main:app --reload
+
+// test on browser http://127.0.0.1:8000
---
+++ main.py
@@ -0,0 +1,25 @@
+import secrets # Using secrets for time waiting checks
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+app = FastAPI()
+
+security = HTTPBasic()
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "test1234") # change it
+    correct_password = secrets.compare_digest(credentials.password, "test1234") # change it
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.get("/users/me")
+def read_current_user(username: str = Depends(get_current_username)):
+    return {"username": username}
